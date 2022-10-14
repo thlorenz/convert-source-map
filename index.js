7455ce412fd832fa0d9be68a5dbd84950f77c5d1
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-10-14 15:09:44 -0700

    add braces in Converter constructor

--- index.js
+++ index.js
@@ -66,11 +66,23 @@ function readFromFileMap(sm, dir) {
 function Converter (sm, opts) {
   opts = opts || {};
 
-  if (opts.isFileComment) sm = readFromFileMap(sm, opts.commentFileDir);
-  if (opts.hasComment) sm = stripComment(sm);
-  if (opts.encoding === 'base64') sm = decodeBase64(sm);
-  else if (opts.encoding === 'uri') sm = decodeURIComponent(sm);
-  if (opts.isJSON || opts.encoding) sm = JSON.parse(sm);
+  if (opts.isFileComment) {
+    sm = readFromFileMap(sm, opts.commentFileDir);
+  }
+
+  if (opts.hasComment) {
+    sm = stripComment(sm);
+  }
+
+  if (opts.encoding === 'base64') {
+    sm = decodeBase64(sm);
+  } else if (opts.encoding === 'uri') {
+    sm = decodeURIComponent(sm);
+  }
+
+  if (opts.isJSON || opts.encoding) {
+    sm = JSON.parse(sm);
+  }
 
   this.sourcemap = sm;
 }
